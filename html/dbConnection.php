60076931891226c87eac9f1b911cb9ae391998e7
Ryan <ryanharper10@gmail.com>
2019-03-19 22:31:02 +1100

    Deleting old files

--- html/dbConnection.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$dbname = "salesscript";
-  $dbConnection = mysqli_connect('localhost', 'paulmiller999', '$3/luknfbaA', 'salesscript')
-     or die('Could not connect: ' . mysqli_error());
- 
-?>
--- html/refund/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: PaymentSettlements/index.php');
-?>
