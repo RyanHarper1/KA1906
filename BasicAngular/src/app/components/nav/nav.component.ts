456fde2106787e28149ca2e097c4ed0b3e361dcb
leejend <39155889+leejend@users.noreply.github.com>
2019-05-23 15:49:21 +1000

    restored cart in nav

--- BasicAngular/src/app/components/nav/nav.component.ts
+++ BasicAngular/src/app/components/nav/nav.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
+
 export class NavComponent implements OnInit {
   
-  constructor(private Auth:AuthService) { }
+  constructor(private Auth:AuthService) { 
+      
+  }
   loggedIn: Boolean;
   ngOnInit() {
     this.Auth.change.subscribe(loggedIn => {
            this.loggedIn = loggedIn;  
       console.log('nav: ' + this.loggedIn)
     });
+    
    
   }
   logout(){
