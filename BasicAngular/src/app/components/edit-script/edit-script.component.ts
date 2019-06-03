718b27a39b9ac36b457ed08b1044547702da8427
Ryan <ryanharper10@gmail.com>
2019-06-04 00:17:26 +1000

    edit script

--- BasicAngular/src/app/components/edit-script/edit-script.component.ts
+++ BasicAngular/src/app/components/edit-script/edit-script.component.ts
@@ -18,7 +18,7 @@ interface answerData {
   idanswer: any
 }
 interface scriptData {
-  questionId: any; 
+  questionId: any;
   scriptId: any;
 }
 @Component({
@@ -40,7 +40,8 @@ export class EditScriptComponent implements OnInit {
   previousAnswerCount = 0;
   scriptData1 = {} as scriptData;
   tempAnswer: any;
-
+  response1: any;
+  loadedScript= false;
   constructor(private editService: EditServiceService, private Http: HttpClient, sanitizer: DomSanitizer, iconRegistry: MatIconRegistry, private Auth: AuthService) {
 
     iconRegistry.addSvgIcon(
@@ -110,8 +111,9 @@ export class EditScriptComponent implements OnInit {
             this.answers[i] = tempAnswers[i]
             console.log('answer responses: ' + this.answers[i].texts);
             this.answer++;
+            
           }
-
+          this.loadedScript = true;
         });
       });
     });
@@ -134,11 +136,12 @@ export class EditScriptComponent implements OnInit {
       this.answer--;
     }
   }
+
   nextQuestion(object, num) {
     this.tempAnswer = this.answers[num].texts;
     console.log('temp answer ' + this.tempAnswer)
     if (this.answers[num].nextQuestionId != null) {
-
+      this.loadedScript = true;
       console.log('previous:' + this.previousAnswers[this.previousAnswerCount])
 
       this.questionId = Number(this.answers[num].questionId)
@@ -166,20 +169,25 @@ export class EditScriptComponent implements OnInit {
 
     }
     else {
+      this.loadedScript = false;
       console.log('no next question linked')
+      this.questionId = Number(this.answers[num].questionId)
+      this.previousAnswers[this.previousAnswerCount] = Number(this.questionId);
+      this.previousAnswerCount++;
       for (let i = 0; i < 9; i++) {
         this.answers[i] = { texts: "", questionId: "", nextQuestionId: null, idanswer: "" }
+
       }
-      
-      this.previousAnswerCount++;
+
+
       this.answer = 1;
-      this.question =" "
+      this.question = " "
 
     }
 
   }
   submit() {
-    if (this.previousAnswerCount == 0) {
+    if (this.loadedScript) {
 
 
       let update = this.Http.post('http://localhost:3000/update-script', { questionId: this.questionId, question: this.question })
@@ -187,26 +195,95 @@ export class EditScriptComponent implements OnInit {
         console.log(result)
         console.log('THISGOING TO WORK')
         for (let j = 0; j < this.answer; j++) {
-          console.log('THIS IS GOING TO WORK: ' + this.answers, this.answers[j].texts)
+          if (this.answers[j].texts != "") {
+            console.log('THIS IS GOING TO WORK: ' + this.answers, this.answers[j].texts)
 
-          let send = this.Http.post<questionData>('http://localhost:3000/editAnswer', { texts: this.answers[j].texts, questionId: this.questionId, nextQuestionId: this.answers[j].nextQuestionId });
-          send.subscribe((res) => {
-            //console.log('THIS IS WHAT HAS RERTUEND: ' + res.result)
-          });
+            let send = this.Http.post<questionData>('http://localhost:3000/editAnswer', { texts: this.answers[j].texts, questionId: this.questionId, nextQuestionId: this.answers[j].nextQuestionId });
+            send.subscribe((res) => {
+              //console.log('THIS IS WHAT HAS RERTUEND: ' + res.result)
+            });
 
+          }
         }
-
       });
 
+
     }
-    else{
-      console.log('ques:' + this.response )
-    this.scriptData1.questionId = this.Auth.scriptData1.questionId;
-    this.scriptData1.scriptId = this.Auth.scriptData1.scriptId;
-  
-        console.log('ques: ' + this.questionId + 'scri' + this.scriptId)
-    this.Auth.submitEditAnswer( this.questionId, this.scriptId, this.answers, this.question, this.tempAnswer)
-    //this.saved = true;
+    else {
+
+      console.log('ques: ' + this.questionId + 'scri' + this.scriptId)
+      //let sql = this.Auth.submitEditAnswer(this.questionId, this.scriptId, this.answers, this.question, this.tempAnswer)
+      console.log('It is trying to submit answer')
+      let request = this.Http.post<questionData>('http://localhost:3000/addQuestion', { texts: this.question, scriptId: this.scriptId });
+      request.subscribe((response1) => {
+        console.log('response1: ' + response1)
+        this.response1 = response1;
+        console.log('response1: ' + response1.questionId)
+        this.scriptData1.questionId = response1.questionId;
+
+        //insert answers
+        for (let i = 0; i < this.answers.length; i++) {
+          if (this.answers[i].texts != "") {
+            let ans = this.Http.post<questionData>('http://localhost:3000/addAnswer', { texts: this.answers[i].texts, questionId: response1.questionId });
+            ans.subscribe((response2) => {
+              this.response = response1
+              console.log('answer responses: ' + response2);
+
+            });
+          }
+        }
+        console.log('selected answer: ' + this.tempAnswer)
+        let update = this.Http.post('http://localhost:3000/updateAnswer', { nextQuestionId: response1.questionId, questionId: this.questionId, texts: this.tempAnswer });
+        update.subscribe((response2) => {
+          this.questionId = response1.questionId
+          let quest = this.Http.post('http://localhost:3000/get-question', { questionId: this.questionId });
+          quest.subscribe((response1) => {
+            console.log('response1: ' + response1[0].texts)
+            this.question = response1[0].texts;
+
+            //get answers
+            let ans = this.Http.post('http://localhost:3000/get-answer', { questionId: this.questionId });
+            ans.subscribe((response2) => {
+              let tempAnswers = response2;
+              this.answer = 0;
+              for (let i = 0; i < tempAnswers.length; i++) {
+                this.answers[i] = tempAnswers[i]
+                console.log('answer responses: ' + this.answers[i].texts);
+                this.answer++;
+              }
+              this.loadedScript = true;
+
+            });
+          });
+        });
+
+
+      });
+      //this.saved = true;
+
     }
+  }
+
+  backQuestion() {
+    this.answer = 0;
+    let quest = this.Http.post('http://localhost:3000/get-question', { questionId: this.previousAnswers[this.previousAnswerCount - 1] });
+    quest.subscribe((response1) => {
+      console.log('response1: ' + response1[0].texts)
+      this.question = response1[0].texts;
+
+      // get answers
+      let ans = this.Http.post('http://localhost:3000/get-answer', { questionId: this.previousAnswers[this.previousAnswerCount - 1] });
+      ans.subscribe((response2) => {
+        let tempAnswers = response2;
+        for (let i = 0; i < tempAnswers.length; i++) {
+          this.answers[i] = tempAnswers[i]
+          console.log('answer responses: ' + this.answers[i].texts);
+          this.answer++;
+        }
+        this.previousAnswerCount--;
+
+      });
+    });
+
   }
 }
