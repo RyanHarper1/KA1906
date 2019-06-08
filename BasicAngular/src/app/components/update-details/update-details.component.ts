f4876e51030e7bf2c6736e0fbb431f37b7585386
Ryan <ryanharper10@gmail.com>
2019-06-08 22:48:58 +1000

    update details page

--- BasicAngular/src/app/components/update-details/update-details.component.ts
+++ BasicAngular/src/app/components/update-details/update-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-update-details',
@@ -8,14 +10,37 @@ import { AuthService } from 'src/app/auth.service';
 })
 
 export class UpdateDetailsComponent implements OnInit {
-  email:any;
-
+  email = new FormControl (this.Auth.email, [Validators.required,Validators.email])
+  curPass = new FormControl ('', [Validators.required])
+  newPass = new FormControl ('', [Validators.required])
+  confirmPass = new FormControl ('', [Validators.required])
+submitted = false;
+errormessage: any;
  
-  constructor(private Auth:AuthService) { }
+  constructor(private Auth:AuthService,private formBuilder: FormBuilder,private Http: HttpClient) { }
 
   ngOnInit() {
-    this.email = this.Auth.email;
+   
 
+
+  }
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+  submit(){
+    if( this.email.valid && this.curPass.valid && this.newPass.valid && this.confirmPass.valid ){
+      alert('valid')
+      this.submitted = true;
+      let post = this.Http.post<any>('http://localhost:3000/updateDetails',{id: this.Auth.id, email: this.email.value, password: this.curPass.value, newPass: this.confirmPass.value})
+      post.subscribe((response) => {
+        this.errormessage = response.result
+      })
+    }
+  }
+  getErrorMessage1() {
+    return this.errormessage
   }
 
 }
