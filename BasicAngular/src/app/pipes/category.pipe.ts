5ffa90b2dfa6bb312edd9108e9e1d6778231da88
jbeckhaus2 <48339530+jbeckhaus2@users.noreply.github.com>
2019-06-11 17:11:00 +1000

    search

--- BasicAngular/src/app/pipes/category.pipe.ts
+++ BasicAngular/src/app/pipes/category.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'category'
+  name: 'categoryPipe'
 })
+
 export class CategoryPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+ transform(value: any, args?: any): any {
+  if(args == null){
+    return value;
+   }
+   return value.filter(
+     list => list.category.toLowerCase().indexOf(args.toLowerCase()) > -1
+  );
+ }
 
 }
